01258780f2f4591427050d35dc76b00adbc692f7
Aung Ko Min <jonahaung@gmail.com>
2020-02-04 13:19:11 +0800

    Initial Commit

---
+++ Natural Language Processing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Natural Language Processing
+//
+//  Created by Aung Ko Min on 4/2/20.
+//  Copyright © 2020 Aung Ko Min. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
